0ad06e84dd78859bbd2794210ae792c62914902a
AhmedAmrhashem <ahmedamrhashm@gmail.com>
2023-09-20 00:01:18 -0700

    betty and other stuff

--- main.c
+++ main.c
@@ -4,46 +4,51 @@
  * main - entry point to shell
  * Return: 0 in success
  */
-
 int main(void)
 {
-        char *buffer = NULL, **argv, *path_name;
-        size_t len = 0;
-        int tmp = 0;
-        path_l *head;
-        char *val;
-        void (*func)(char **);
+	char *buffer = NULL;
+	size_t len = 0;
+	int tmp = 0;
+	path_l *head;
+	char *val, **argv, *path_name;
+	void (*func)(char **);
 
-        while (tmp != EOF)
-        {
-                _puts("$ ");
-                tmp = getline(&buffer, &len, stdin);
-                argv = string_split(buffer);
-                hash_handle(argv);
-                if (argv && argv[0])
-                {
-                        val = _getenv("PATH");
-                        head = link_path(val);
-                        path_name = _which(head, argv[0]);
-                        func = builtin_func(argv);
-                        if (func)
-                        {
-                                func(argv);
-                        }
-                        else if (!path_name)
-                                execute_p(argv);
-                        else if(path_name)
-                        {
-                                argv[0] = path_name;
-                                execute_p(argv);
-                                free(argv[0]);
-                        }
-                }
-                else
-                        continue;
-
-        }
-        return (0);
+	while (tmp != EOF)
+	{
+		_puts("$ ");
+		tmp = getline(&buffer, &len, stdin);
+		argv = string_split(buffer);
+		hash_handle(argv);
+		if (argv && argv[0])
+		{
+			val = _getenv("PATH");
+			head = link_path(val);
+			path_name = _which(head, argv[0]);
+			func = builtin_func(argv);
+			if (func)
+			{
+				func(argv);
+			}
+			else if (!path_name)
+			{
+				execute_p(argv);
+			}
+			else if(path_name)
+			{
+				argv[0] = path_name;
+				execute_p(argv);
+				free(argv[0]);
+			}
+		}
+		else
+		{
+			continue;
+		}
+	}
+	free(buffer);
+	free(argv);
+	free_list(path_l *head);
+	return (0);
 }
 
 /**
@@ -53,20 +58,38 @@ int main(void)
  */
 void hash_handle(char **argv)
 {
-        int i = 0;
+	int i = 0;
 
-        while (argv[i])
-        {
-                if (argv[i][0] == '#')
-                {
-                        while (argv[i])
-                        {
-                                argv[i] = NULL;
-                                free(argv[i]);
-                                i++;
-                        }
-                        return;
-                }
-                i++;
-        }
+	while (argv[i])
+	{
+		if (argv[i][0] == '#')
+		{
+			while (argv[i])
+			{
+				argv[i] = NULL;
+				free(argv[i]);
+				i++;
+			}
+			return;
+		}
+		i++;
+	}
+}
+
+/**
+ * free_list - frees a linked list
+ * @head: start node
+ * Return: void
+ */
+void free_list(path_l *head)
+{
+	path_l *remove = head;
+
+	while (head)
+	{
+		remove = head->next;
+		free(head->str);
+		free(head);
+		head = remove;
+	}
 }
--- write_functions.c
+++ write_functions.c
@@ -5,7 +5,6 @@
  * @c: char to be printed
  * Return: 1 on success or -1 on failure
  */
- 
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
@@ -15,8 +14,7 @@ int _putchar(char c)
  * _puts - printd a string 
  * @str: char pointer
  * Return: void
- */
- 
+ */ 
 void _puts(char *str)
 {
 	int i = 0;
