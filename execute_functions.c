7828e8e8b58c02577cd17ae2b7fe07298f686c5d
Eng-Phenomena <eng.phenomena@gmail.com>
2023-09-20 15:37:00 -0700

    getenv

--- execute_functions.c
+++ execute_functions.c
@@ -50,48 +50,30 @@ int shell_execute(char **args)
 }
 
 /**
- * _getenv - Get the string of a environtment varible
- * @name: The name of the varible to find
- * Return: return the string if founded
+ * _getenv - getting the address of desired name in env
+ * @name: string to be searched for
+ * Return: address of desired path or variable
  */
 char *_getenv(const char *name)
 {
-	int i = 0, j = 0, k = 0;
-	int len = _strlen((char *)name);
-	char **env = environ;
-	char *str = malloc(sizeof(char) * 150);
+	char **env = environ, *namePtr;
+	char *var, *value;
 
-	if (!str)
+	for (; *env != NULL; env++)
 	{
-		return (NULL);
-	}
-	if (!name)
-	{
-		free(str);
-		return (NULL);
-	}
-	while (env[i][j] != '\0')
-	{
-		if (_strncmp(env[i], (char *)name, len) != 0)
+		var = *env;
+		value = var;
+		namePtr = (char *)name;
+
+		while (*namePtr != '\0' && *value != '=' && *value == *namePtr)
 		{
-			i++;
+			namePtr++;
+			value++;
 		}
-		else
-		{
-			if (env[i][len] == '=')
-			{
-				while (env[i][len + j] != '\0')
-				{
-					str[k] = (env[i][len + j + 1]);
-					k++;
-					j++;
-				}
-				return ((char *)str);
-			}
-		}
+		if (*namePtr == '\0' && *value == '=')
+			return (value + 1);
 	}
 
-	free(str);
 	return (NULL);
 }
 
