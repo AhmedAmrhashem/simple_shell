503e0e25330977d0148449d4fe7cb75bb35845ff
AhmedAmrhashem <ahmedamrhashm@gmail.com>
2023-09-20 09:13:53 -0700

    Betty is Wife

--- string_functions.c
+++ string_functions.c
@@ -84,7 +84,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (malloc(new_size));
 
 	if (new_size == old_size)
-		 return (ptr);
+		return (ptr);
 
 	if (new_size == 0 && ptr != NULL)
 	{
